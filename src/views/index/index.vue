b152c713ed59b0ea38ddb09ac6efc914764e2762
殷魄 <dvyinpo@163.com>
2020-09-10 15:35:49 +0800

    first commit

---
+++ src/views/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    这是index
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+};
+</script>
+
+<style scoped lang="less">
+@import "index";
+</style>
